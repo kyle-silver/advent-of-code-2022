2fe3563e928b96163ac176edb76e02b0acc6ac11
Kyle Silver <kyleAsilver@gmail.com>
2022-12-01 20:49:16 -0500

    simplify

--- tests/day01.rs
+++ tests/day01.rs
@@ -5,34 +5,6 @@ use std::collections::BinaryHeap;
 const INPUT: &str = include_str!("res/01.txt");
 
 #[test]
-fn part1() {
-    let answer: u32 = INPUT
-        .split("\n\n")
-        .map(|raw| {
-            raw.lines()
-                .map(|calories| calories.parse::<u32>().unwrap())
-                .sum()
-        })
-        .max()
-        .expect("list should have at least one item!");
-    println!("Day 1, part 1: {answer}");
-}
-
-#[test]
-fn part2() {
-    let heap: BinaryHeap<u32> = INPUT
-        .split("\n\n")
-        .map(|raw| {
-            raw.lines()
-                .map(|calories| calories.parse::<u32>().unwrap())
-                .sum()
-        })
-        .collect();
-    let answer: u32 = heap.iter().take(3).sum();
-    println!("Day 1, part 2: {answer}");
-}
-
-#[test]
 fn day1() {
     let heap: BinaryHeap<u32> = INPUT
         .split("\n\n")
